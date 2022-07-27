26b73f76d34ef7655ae750a81ca34634ca92b540
Bezawada, GopalaKrishna - Dell Team <gopala_krishna_bezaw@dellteam.com>
2022-07-27 12:50:11 +0530

    Modified password validation code

--- src/main/java/com/ibm/registration/userregistration/validation/Utility.java
+++ src/main/java/com/ibm/registration/userregistration/validation/Utility.java
@@ -3,28 +3,20 @@ package com.ibm.registration.userregistration.validation;
 import java.util.regex.Pattern;
 
 public class Utility {
-	
-	
-	public static boolean isValid(String passwordhere) {
 
-	    Pattern specailCharPatten = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
-	    Pattern UpperCasePatten = Pattern.compile("[A-Z ]");
-	    Pattern digitCasePatten = Pattern.compile("[0-9 ]");
+	public static boolean isValid(String password) {
 
-	    boolean flag=true;
-	    
-	    if (!specailCharPatten.matcher(passwordhere).find()) {
-	        flag=false;
-	    }
-	    else if (!UpperCasePatten.matcher(passwordhere).find()) {
-	        flag=false;
-	    }
-	    else if (!digitCasePatten.matcher(passwordhere).find()) {
-	        flag=false;
-	    }
+		boolean flag = true;
 
-	    return flag;
+		Pattern passwordPattern = Pattern.compile("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#@$?]).{8,}$");
+
+		if (!passwordPattern.matcher(password).find()) {
+			flag = false;
+		}
+
+		return flag;
 
 	}
+
 
 }
